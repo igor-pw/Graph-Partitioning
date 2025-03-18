6392b32c85e0ee6a61e24dedc27568f6989e6c44
igor-pw <01197849@pw.edu.pl>
2025-03-18 01:28:46 +0100

    Mnorzenie macierzy i wektorow, wektor poczatkowy

--- src/G-Cut.c
+++ src/G-Cut.c
@@ -11,6 +11,7 @@ int main(int argc, char **argv)
 
 	int n = 0;
 	int A_size = 0;
+	int D_norm = 0;
 
 	fscanf(in, "%d\n", &n);
 	
@@ -18,17 +19,16 @@ int main(int argc, char **argv)
 		return 2;
 
 	double **A_matrix = create_A_matrix(n, in, &A_size);
-	double **Diag = diagonal_matrix(A_matrix, A_size);
+	double **Diag = diagonal_matrix(A_matrix, A_size, &D_norm);
+
 	double **Sub = subtract_matrix(Diag, A_matrix ,A_size);
-	print_matrix(A_matrix, A_size);
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	print_matrix(Diag, A_size);
-	printf("\n");
-	printf("\n");
 	printf("\n");
 	print_matrix(Sub, A_size);
+	printf("\n");
+	double *initial_vec = create_initial_vec(Diag, D_norm, A_size);
+
+	for(int i = 0; i < A_size; i++)
+	      printf("%g ", initial_vec[i]);	
 
 	return 0;
 }
--- src/headers/matrix.h
+++ src/headers/matrix.h
@@ -6,8 +6,11 @@
 #include <math.h>
 
 double **create_A_matrix(int n, FILE *in, int *nr_nodes);
-double **diagonal_matrix(double **matrix, int A_size);
+double **diagonal_matrix(double **matrix, int A_size, int *D_norm);
 double **subtract_matrix(double **matrix1, double **matrix2, int A_size);
 void print_matrix(double **matrix, int n);
+double *multiply_mtx_by_vec(double **matrix, int n, double *vec);
+double multiply_vec_by_vec(double *vec, double *vecT, int n);
+double *create_initial_vec(double **D_matrix, int D_norm, int n);
 
 #endif
